619079f3a3d50e22a0266f4095f3337586aa86e9
Alexis Flipo <alexis.flipo1@gmail.com>
2021-06-28 11:16:59 +0200

    predictions

--- stats_model/visualisation.py
+++ stats_model/visualisation.py
@@ -29,7 +29,7 @@ def if_stationarity(timeseries):
     plt.show(block=False)
     
     # Test Dickey–Fuller :
-    result = adfuller(timeseries['Opening_Price_ETH'])
+    result = adfuller(timeseries['Open'])
     print('Statistiques ADF : {}'.format(result[0]))
     print('p-value : {}'.format(result[1]))
     print('Valeurs Critiques :')
@@ -44,6 +44,8 @@ def log_plot(df):
 def vis_predict(pred, test, legende):
     np.exp(pred).plot(legend=True)
     np.exp(test[legende]).plot(legend=True)
+    plt.ylabel("Cours de l'ETH(USD)")
+    plt.title("Prédiction du cours de l'ETH(USD)")
     plt.show()
 
 def predict_future_price_ETH(model_fit, test_data):
@@ -51,11 +53,22 @@ def predict_future_price_ETH(model_fit, test_data):
     start_date = input('Date de début de prédiction qui ne dépasse pas le 28 aout 2019 :')
     end_date = input('Date de fin de prédiction :')
     pred = model_fit.predict(start=start_date,end=end_date,typ='levels').rename('ARIMA Predictions')
-    np.exp(pred).plot(legend=True)
-    np.exp(test_data['Opening_Price_ETH']).plot(legend=True, label="Prix réel de l'ETH", color='green')
+    np.exp(pred).plot(legend=True, color='red')
+    np.exp(test_data['Opening_Price_ETH']).plot(legend=True, label="Prix réel de l'ETH", color='green', style='--')
     plt.xlim(start_date, end_date)
     plt.ylim(0, np.exp(test_data['Opening_Price_ETH'][end_date]))
     plt.title("Prediction du prix de l'ETH en fonction de son cours")
     plt.xlabel("Date")
     plt.ylabel("Prix de l'ETH")
     return plt.show()
+def predict_tomorrow_ETH_price(model_fit, df_log):
+    plt.figure(figsize=(12,8))
+    pred=model_fit.predict(start='2020-06-15',end='2021-08-15',typ='levels').rename('ARIMA Predictions')
+    np.exp(pred).plot(legend=True, color='red')
+    np.exp(df_log['Open'][len(df_log)-len(pred):]).plot(legend=True, label='Real values', color='blue', style='--')
+    plt.ylabel("Cours de l'ETH(USD)")
+    plt.title("Prédiction du cours de l'ETH(USD)")
+    plt.show()
+
+def predict_date(start_date, end_date, model_fit):
+    print(f"Les prédictions du cours de l'ETH pour le {end_date} est de {np.exp(model_fit.predict(start=start_date,end=end_date,typ='levels')).values[-1]} dollars")
